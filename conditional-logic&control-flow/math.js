ebe79069bcd2b8dd7172921784651cb7ce82aeb3
helmichehasni <143876217+helmichehasni@users.noreply.github.com>
2024-02-06 20:21:39 +0800

    Create math.js

---
+++ conditional-logic&control-flow/math.js
@@ -0,0 +1,32 @@
+//addition
+let val1 = 3+2;
+console.log(`Addition: ${val1}`);
+
+//substraction
+let val2 = 3-2
+console.log(`Subtration: ${val2}`);
+
+//multiplication
+let val3 = 3 * 2;
+console.log(`Multiplication: ${val3}`);
+
+//division
+let val4 = 3/2
+let val5 = 9/2
+console.log(`Division: ${val4} ${val5}`);
+
+//remainder
+let val6 = 9%2
+console.log(`Remainder: ${val6}`);
+
+//exponentiation
+let val7 = 3 ** 2;
+console.log(`exponent: ${val7}`);
+
+//Increment Decrement
+let val8 = 1;
+++val8;
+console.log(`Increment: ${val8}`);
+let val9 = 1;
+--val9;
+console.log(`Decrement: ${val9}`);
